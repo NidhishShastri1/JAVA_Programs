fb5fd669ee84b1951edbfae4357c170efe83d22b
NidhishShastri1 <nidhish.shastri1234@gmail.com>
2025-11-01 12:12:44 +0530

    Product of Numbers

---
+++ src/ProductOfTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ProductOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int a = sc.nextInt();
+        System.out.println("Enter 2st number");
+        int b =  sc.nextInt();
+        System.out.println("The Sum is: " +(a*b));
+    }
+}
