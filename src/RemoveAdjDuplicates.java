f31c9d6dac1cae18cb0926b815baefb0aaeeec60
NidhishShastri1 <nidhish.shastri1234@gmail.com>
2025-11-03 17:34:21 +0530

    remove adjacent words

--- src/RemoveAdjDuplicates.java
+++ src/RemoveAdjDuplicates.java
@@ -1,6 +1,6 @@
 public class RemoveAdjDuplicates {
     public static void main(String[] args) {
-        String str = "aabbcddeef";
+    /*    String str = "aabbcddeef";
         char[] c = str.toCharArray();
         for(int i = 0 ; i<c.length-1; i++){
             if (c[i]!=c[i+1]){
@@ -8,5 +8,14 @@ public class RemoveAdjDuplicates {
             }
         }
         System.out.print(c[c.length-1]);
+    */
+        String str = " hi hi hi hello hello hello how r r u u";
+        String[] s = str.split(" ");
+        for (int i = 0; i<s.length-1;i++){
+            if(!s[i].equals(s[i+1])){
+                System.out.print(s[i]+" ");
+            }
+        }
+        System.out.print(s[s.length-1]);
     }
 }
