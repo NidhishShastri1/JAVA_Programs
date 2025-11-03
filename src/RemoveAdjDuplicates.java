26e54c426aa9a856342f258008648490850c3685
NidhishShastri1 <nidhish.shastri1234@gmail.com>
2025-11-03 17:47:27 +0530

    remove adjacent and duplicate

--- src/RemoveAdjDuplicates.java
+++ src/RemoveAdjDuplicates.java
@@ -1,6 +1,6 @@
 public class RemoveAdjDuplicates {
     public static void main(String[] args) {
-    /*    String str = "aabbcddeef";
+/*        String str = "aabbcddeef";
         char[] c = str.toCharArray();
         for(int i = 0 ; i<c.length-1; i++){
             if (c[i]!=c[i+1]){
@@ -8,7 +8,7 @@ public class RemoveAdjDuplicates {
             }
         }
         System.out.print(c[c.length-1]);
-    */
+
         String str = " hi hi hi hello hello hello how r r u u";
         String[] s = str.split(" ");
         for (int i = 0; i<s.length-1;i++){
@@ -16,6 +16,14 @@ public class RemoveAdjDuplicates {
                 System.out.print(s[i]+" ");
             }
         }
-        System.out.print(s[s.length-1]);
+        System.out.print(s[s.length-1]);*/
+        int[] a = {1,1,1,12,2,2,4,5,6,6,7};
+        for(int i = 0 ; i<a.length-1; i++){
+            if (a[i]!=a[i+1]){
+                System.out.print(a[i] + " ");
+            }
+        }
+        System.out.print(a[a.length-1]);
+
     }
 }
